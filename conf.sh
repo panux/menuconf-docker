d761fc45906cd6236d52884aeb09d02b6085c7c9
Jaden Weiss <jadr2ddude@gmail.com>
2017-10-21 07:48:04 -0400

    fix args

--- conf.sh
+++ conf.sh
@@ -1,6 +1,6 @@
 set -e
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
 	echo "Missing arguments"
 fi
 
@@ -10,10 +10,10 @@ mv linux-$1 linux
 if [ -f "$2" ]; then
 	cp $2 linux/.config
 else
-	if [ $2 = x86 ]; then
+	if [ $3 = x86 ]; then
 		make -C linux ARCH=i386 defconfig
 	else
-		make -C linux ARCH=$2 defconfig
+		make -C linux ARCH=$3 defconfig
 	fi
 fi
 make -C linux menuconfig
