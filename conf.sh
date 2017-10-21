54343547cec3152ef865b672a4476a55f2564cab
Jaden Weiss <jadr2ddude@gmail.com>
2017-10-21 07:46:25 -0400

    Set arch in builds

--- conf.sh
+++ conf.sh
@@ -1,11 +1,20 @@
 set -e
+
+if [ $# -lt 2 ]; then
+	echo "Missing arguments"
+fi
+
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$1.tar.xz
 tar -xf linux-$1.tar.xz
 mv linux-$1 linux
 if [ -f "$2" ]; then
 	cp $2 linux/.config
 else
-	make -C linux defconfig
+	if [ $2 = x86 ]; then
+		make -C linux ARCH=i386 defconfig
+	else
+		make -C linux ARCH=$2 defconfig
+	fi
 fi
 make -C linux menuconfig
 cat linux/.config > $2
